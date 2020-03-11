557b385b05076d2c3e618c5607ea1e57203ea9d6
Yogev Lidor <yogevl@safeblocks.io>
2020-03-11 12:36:22 +0200

    sc 2 io

--- Java-Fundamentals/src/ac/elevation/spotchecks/io/SpotCheck2.java
+++ Java-Fundamentals/src/ac/elevation/spotchecks/io/SpotCheck2.java
@@ -1,8 +1,10 @@
 package ac.elevation.spotchecks.io;
 
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Yogev Lidor
@@ -11,14 +13,20 @@ import java.io.IOException;
 public class SpotCheck2 {
 
     public static void main(String[] args) {
-        try (FileInputStream fn = new FileInputStream("c:\\data\\yogibear.jfif");
-             FileOutputStream fo = new FileOutputStream("c:\\data\\copied-image.jfif")) {
-            int k;
-            while ((k = fn.read()) != -1) {
-                fo.write((byte) k);
+        List<String> namesAndPhones = new ArrayList<>();
+        namesAndPhones.add("Ela" + ":" + "054303020");
+        namesAndPhones.add("Yael" + ":" + "052020587");
+        namesAndPhones.add("Natan" + ":" + "056892487");
+        namesAndPhones.add("Gali" + ":" + "053629628");
+
+        try (FileOutputStream fos = new FileOutputStream("c:\\data\\output.txt");
+             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fos)) {
+            for (String entry : namesAndPhones) {
+                outputStreamWriter.write(entry + "\n");
             }
         } catch (IOException e) {
             System.out.println(e);
         }
     }
+
 }
