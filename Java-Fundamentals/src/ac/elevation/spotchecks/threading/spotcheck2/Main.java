42e06146e58478072d8edc869bccad6432d91b80
Yogev Lidor <yogevl@safeblocks.io>
2020-03-12 00:22:41 +0200

    sc 2 threading

--- Java-Fundamentals/src/ac/elevation/spotchecks/threading/spotcheck2/Main.java
+++ Java-Fundamentals/src/ac/elevation/spotchecks/threading/spotcheck2/Main.java
@@ -7,11 +7,39 @@ package ac.elevation.spotchecks.threading.spotcheck2;
 public class Main {
 
     public static void main(String[] args) {
-            Counter counter = new Counter();
-            Thread threadA = new CounterThread(counter);
-            Thread threadB = new CounterThread(counter);
+        for (int k = 0; k < 5000; k++) {
+            runCounter();
+        }
+    }
 
-            threadA.start();
-            threadB.start();
+    private static void runCounter() {
+        int numberOfThreads = 10;
+        int expectedCounterAmount = 150;
+        Counter counter = new Counter();
+
+        CounterThread[] workers = new CounterThread[numberOfThreads];
+        for (int i = 0; i < numberOfThreads; i++) {
+            workers[i] = new CounterThread(counter);
+        }
+
+        //start the threads
+        for (int i = 0; i < numberOfThreads; i++) {
+            workers[i].start();
+        }
+
+        /* Wait for all threads to terminate. */
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            try {
+                workers[i].join();
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+
+        // print if value is wrong
+        if (counter.getCount() != expectedCounterAmount) {
+            System.out.println("Counter value is wrong!!! " + counter.getCount());
+        }
     }
 }
